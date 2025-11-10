389a0e97e147a21afac9c8e4694111dd60dc3db4
yolanda <yogomgon2014@gmail.com>
2025-11-09 20:18:18 -0500

    Ejercicio completo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -4,7 +4,7 @@ chocolate=4000
 vainilla = 3500
 topping = "1000"
 
-"Bienbenidos heladeria Frosty"
+print ("Bienbenidos heladeria Frosty")
 
 sabordehelado=  input ("sabor de healdo que desea  :")
 
@@ -20,7 +20,16 @@ if sabordehelado == "chocolate" :
     else : 
         print ("valor a pagar helado de chocolate",chocolate )
 
+elif sabordehelado == "vainilla" :
+    print ("sabor elegido vainilla valor:.", vainilla)
 
+    adiccionesvainilla = input ("desea adicioanr topping por valor de " + topping + "si o no")
 
+    if adiccionesvainilla == "si":
+        print("valor a pagar helado mas topping",vainilla + 1000)
 
+    else :
+        print ("valor a pagar helado vainilla", vainilla)
 
+else:
+    print ("Error, sabor invalido, por favor ingresa entre vainilla y chocolate")
